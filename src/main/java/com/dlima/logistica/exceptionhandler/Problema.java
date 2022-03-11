b516706de987ca3dd64ad265e9d00435c3625af3
Marcos Lima <48837929+MarcosACLima@users.noreply.github.com>
2022-03-11 03:30:50 -0300

    Implementing Exception Handler

---
+++ src/main/java/com/dlima/logistica/exceptionhandler/Problema.java
@@ -0,0 +1,28 @@
+package com.dlima.logistica.exceptionhandler;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Problema {
+	
+	private Integer status;
+	private LocalDateTime dataHora;
+	private String titulo;
+	private List<Campo> campo;
+	
+	@AllArgsConstructor
+	@Getter
+	public static class Campo {
+		
+		private String nome;
+		private String mensagem;
+		
+	}
+
+}
